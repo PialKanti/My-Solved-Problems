d06194863c9e4b80083d5814269e8c0ee6722c82
PialKanti <pialkanti2012@gmail.com>
2017-09-20 14:50:54 +0600

    Added new Java file

---
+++ src/HackerRank/Strings/TwoCharacters.java
@@ -0,0 +1,17 @@
+package HackerRank.Strings;
+
+import java.util.Scanner;
+
+/**
+ * Problem name: Two Characters
+ *
+ * @author: Pial Kanti Samadder <pialkanti2012@gmail.com>
+ * Date: 9/20/2017
+ * Time: 1:22 PM
+ */
+public class TwoCharacters {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+    }
+}
