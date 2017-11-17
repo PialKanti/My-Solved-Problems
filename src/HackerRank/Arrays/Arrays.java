35a6a2bcf8ae8dce900ca7551c44cddae8fca625
PialKanti <pialkanti2012@gmail.com>
2017-11-17 15:59:30 +0600

    Added Dynamic Arrays solution in hackerrank

--- src/HackerRank/Arrays/Arrays.java
+++ src/HackerRank/Arrays/Arrays.java
@@ -1,5 +1,24 @@
 package HackerRank.Arrays;
 
-public class Arrays {
+import java.util.Scanner;
 
+/**
+ * Problem name: Arrays-DS
+ * 
+ * @author: Pial Kanti Samadder <pialkanti2012@gmail.com>
+ * Date: Nov 15, 2017
+ * Time: 5:17:17 PM
+ */
+
+public class Arrays {
+	public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+		String s = "";
+		int n = in.nextInt();
+		for (int i = 0; i < n; i++) {
+			s = in.nextInt()+" "+s;
+		}
+		System.out.println(s);
+    }
 }
---
+++ src/HackerRank/Arrays/DynamicArray.java
@@ -0,0 +1,38 @@
+package HackerRank.Arrays;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Problem name: Dynamic Array 
+ * 
+ * @author: Pial Kanti Samadder <pialkanti2012@gmail.com>
+ * Date: Nov 16, 2017
+ * Time: 5:17:17 PM
+ */
+
+public class DynamicArray {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int q = in.nextInt();
+		ArrayList<Integer>[] seq = new ArrayList[n];
+		for(int i = 0;i <n ; i++){
+			seq[i] = new ArrayList<Integer>();
+		}
+		int lastAns = 0;
+		for(int i = 0; i < q; i++){
+			int s = in.nextInt();
+			int x = in.nextInt();
+			int y = in.nextInt();
+			int index = (x ^ lastAns) % n;
+			if(s == 1){
+				seq[index].add(y);
+			}else if(s == 2){
+				lastAns = seq[index].get(y % (seq[index].size()));
+				System.out.println(lastAns);
+			}
+		}
+	}
+
+}
